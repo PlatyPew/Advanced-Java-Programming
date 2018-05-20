a2aada83dda25d4c8d15985ddf8bab6e7d85dbad
PlatyPew <daryl.limkz@gmail.com>
2018-05-21 00:58:13 +0800

    Probably not the final commit

--- src/mrtRoutes/MrtServiceMain.java
+++ src/mrtRoutes/MrtServiceMain.java
@@ -1,8 +1,7 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+@author Chun Yu
+*/
+
 package mrtRoutes;
 
 import java.io.*;
@@ -11,15 +10,8 @@ import java.util.*;
 import javax.swing.ImageIcon;
 import javax.swing.JOptionPane;
 
-/**
- *
- * @author chuny
- */
 public class MrtServiceMain {
 
-    /**
-     * @param args the command line arguments
-     */
     public static HashMap<String, String> num = new LinkedHashMap<String, String>();// key is cc11 etc
     public static HashMap<String, ArrayList<String>> desc = new LinkedHashMap<String, ArrayList<String>>();// key is name 
 
