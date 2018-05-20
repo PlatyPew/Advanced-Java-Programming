2d4540d18e1a0c5c0314026d2e9d48691bf9ef85
PlatyPew <daryl.limkz@gmail.com>
2018-05-21 01:08:25 +0800

    List mrt features

--- src/Main.java
+++ src/Main.java
@@ -17,7 +17,16 @@ Has validation when choosing stations
 Allows users to stop program if they press 'cancel' on the JOptionPane
 
 MRT Features:
-
+Shows where you are on the map when you search for the line
+Allows for more than one interchange between routes
+Image for each respective mrt lines
+Find fastest route to destination from all possible routes
+Returns number of stations between all stops
+User can search using either station desc or station num on same seach bar
+Drop down table for transfer stations
+Allows users to stop program if they press 'cancel' on the JOptionPane
+Has validation when choosing stations
+Addition of NE line
 */
 
 import busRoutes.*;
